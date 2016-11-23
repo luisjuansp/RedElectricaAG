5c458d23840a32b9fc5297cbed14f4492034c5ce
Luis Juan Sanchez <luisjuansp@gmail.com>
2016-11-22 21:11:04 -0600

    merge

--- RedElectricaAG.m
+++ RedElectricaAG.m
@@ -8,8 +8,6 @@ hijo = [3 3 1 1 1 3];
 %       0 1 2 3 4 5 6
 prod = [0 10 5 3 4 9 8];
 
-hijo = breakLoops(hijo);
-
 % Coordenadas del nodo i-1
 %     0   1   2   3   4    5    6
 C = [0 0;3 2;6 2;6 0;3 -2;6 -2;9 -2];
